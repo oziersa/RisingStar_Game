49d55f2083ff3796395a5cf03aceb2bed08e9490
oziersa <samozier@aol.com>
2020-04-06 17:21:05 -0500

    Updated Project

---
+++ Project_Whole/Platform_Project/Platform_Project/Assets/Scripts/DynoCam.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynoCam : MonoBehaviour
+{
+    PlayerMotion player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMotion>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newCamPos = new Vector2(player.transform.position.x, player.transform.position.y);
+        transform.position = new Vector3(newCamPos.x, newCamPos.y, transform.position.z);
+    }
+}
