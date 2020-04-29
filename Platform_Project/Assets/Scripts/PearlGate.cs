892452ec2614de2a403c28796fffab6bec811b68
ellie4712 <57506947+ellie4712@users.noreply.github.com>
2020-04-28 22:37:31 -0500

    My level (not quite finished)
    I want to make sure it will actually commit....

--- Platform_Project/Assets/Scripts/PearlGate.cs
+++ Platform_Project/Assets/Scripts/PearlGate.cs
@@ -6,14 +6,12 @@ public class PearlGate : MonoBehaviour
 {
     //Pearls to catch
     [SerializeField] public int pearlyGate;
-    [SerializeField] AudioClip door;
 
     // Update is called once per frame; Destroy when pearls are found
     void Update()
     {
         if(pearlyGate <= 0)
         {
-            AudioSource.PlayClipAtPoint(door, gameObject.transform.position);
             Destroy(gameObject);
         }
     }
---
+++ Platform_Project/Assets/Scripts/SpikeDamage.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeDamage : MonoBehaviour
+{
+
+	// when player touches spike,
+	// they should die and go back to last checkpoint
+	// the same as if they touched an enemy. 
+	
+	private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<PlayerMotion>())
+        {
+            PlayerMotion player = collision.GetComponent<PlayerMotion>();
+            player.PlayerPerish();
+        }
+    }
+
+}
