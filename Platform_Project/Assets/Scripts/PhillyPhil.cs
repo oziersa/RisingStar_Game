cd6bc937a019d058883c5b938cb0dd3b2f37729b
oziersa <samozier@aol.com>
2020-04-29 21:00:08 -0500

    4/29

---
+++ Platform_Project/Assets/Scripts/PhillyPhil.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhillyPhil : MonoBehaviour
+{
+    [SerializeField] Dialogue dialogue;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<PlayerMotion>())
+        {
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+            Destroy(gameObject.GetComponent<PhillyPhil>());
+        }
+    }
+}
---
+++ Platform_Project/Assets/Scripts/TextScroll.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TextScroll : MonoBehaviour
+{
+    [SerializeField] public Animator anim;
+    [SerializeField] public Image blackOut;
+
+    private void Start()
+    {
+        anim.SetBool("End", false);
+    }
+
+    private void Update()
+    {
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x,
+            gameObject.transform.position.y + 1f, gameObject.transform.position.z);
+
+        if(gameObject.transform.position.y > 700)
+        {
+            Debug.Log("Over");
+            //StartCoroutine(sceneFade());
+        }
+    }
+
+    IEnumerator sceneFade()
+    {
+        //Make transition
+        anim.SetBool("End", true);
+
+        yield return new WaitUntil(() => blackOut.color.a == 1f);
+
+        SceneManager.LoadScene(0);
+    }
+}
