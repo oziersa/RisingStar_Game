974105edb50e77b2cdc60e63885bda27f0817209
oziersa <samozier@aol.com>
2020-04-30 16:18:23 -0500

    Finally

---
+++ Platform_Project/Assets/SliderSetting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SliderSetting : MonoBehaviour
+{
+    public Text volPercent;
+    public AudioMixer mixer;
+    public Slider Slider;
+
+    //Cached
+    int vol;
+
+    public void SetVol (float sliderVolume)
+    {
+        mixer.SetFloat("MasterVol", Mathf.Log10(sliderVolume)*20);
+    }
+
+    private void Update()
+    {
+        vol = (int) (Slider.value / Slider.maxValue * 100f);
+        volPercent.text = vol.ToString() + " %";
+    }
+}
