c4989b5e838cfdcfe903f4a1c95a42450fa6b13d
tjrex-99 <57365711+tjrex-99@users.noreply.github.com>
2020-04-29 23:50:00 -0500

    failed attempt at beginning dialogue

---
+++ Platform_Project/Assets/Scripts/OpeningDialogueTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpeningDialogueTrigger : MonoBehaviour
+{
+    [SerializeField] Dialogue dialogue;
+    private bool first = true;
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerMotion>() && Input.GetButton("Fire1") && first)
+        {
+            TriggerDialogue();
+            first = false;
+        }
+
+        else if (collision.GetComponent<PlayerMotion>() && Input.GetButton("Fire1"))
+        {
+            FindObjectOfType<DialogueManager>().ShowNextSentence();
+        }
+    }
+}
